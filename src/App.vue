7a2022a183c5d0d96a046f0fa27e6f2b70de69a9
Alejandro Cordero Diadosa <alejandro.c9.d6@gmail.com>
2024-02-14 11:37:42 +0100

    responsive fix

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ import Footer from "./components/aside/Footer.vue";
 <style lang="scss" scoped>
 @import "./assets/utilities.scss";
 .main {
-  width: 75%;
+  width: 60%;
   height: 40rem;
   display: flex;
   align-items: center;
