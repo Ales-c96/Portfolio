6b6e7309ef319bec2b9e91970790fcfe38526bae
Alejandro Cordero Diadosa <alejandro.c9.d6@gmail.com>
2024-02-25 17:19:28 +0100

    gtag analytics

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import "@fortawesome/fontawesome-free/css/all.css";
-import "./assets/reset.css";
-import "./assets/style.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueGtag from "vue-gtag";
+
+import "@fortawesome/fontawesome-free/css/all.css";
+import "./assets/reset.css";
+import "./assets/style.css";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(VueGtag, { config: { id: "G-WVB29M5BGS" } });
 
 app.mount("#app");
