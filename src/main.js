71af41b952f01e4c68f6291a5078d9342ab5f491
Alejandro Cordero Diadosa <alejandro.c9.d6@gmail.com>
2024-02-29 11:29:53 +0100

    gtag unisntall

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import VueGtag from "vue-gtag";
 
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./assets/reset.css";
@@ -10,6 +9,5 @@ import "./assets/style.css";
 const app = createApp(App);
 
 app.use(router);
-app.use(VueGtag, { config: { id: "G-WVB29M5BGS" } });
 
 app.mount("#app");
